f14ac65bcfd27f4e6fe9c6402ab21de8b35a01a7
Sevastopol12 <chkalov253@gmail.com>
2025-11-25 12:15:50 +0700

    fix(database-connection): resolve unexpected connection drop

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,9 @@
 import os
 from sqlalchemy import create_engine
 
-
-
 class Connection:
     connection_string = os.environ["DATABASE_URI"]
-    engine = create_engine(url=connection_string)
+    engine = create_engine(url=connection_string, pool_pre_ping=True)
 
 
 db_connection = Connection()
